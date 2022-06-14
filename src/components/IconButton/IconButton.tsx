94f674b0e1e2590cf03da074e89ad860624d76d3
Pabl1k <poiumazaya@gmail.com>
2022-06-14 18:37:44 +0300

    Added Telegram QR code on button hover

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, {FC, useState} from "react";
 import Icon, { IconName } from "../Icon/Icon";
 import "./IconButton.scss";
 
@@ -8,15 +8,28 @@ type Props = {
 };
 
 const IconButton: FC<Props> = ({ iconName, path }) => {
+  const [hovered, setHovered] = useState(false);
+
+  const mouseListener = (e: boolean) => {
+    if (iconName === "telegram") {
+      setHovered(e)
+    }
+  }
+
   return (
-    <a
-      className="circle-with-icon"
-      href={path}
-      target="_blank"
-      rel="noreferrer"
-    >
-      <Icon name={iconName} height="20" width="20" />
-    </a>
+    <>
+      <a
+        className="circle-with-icon"
+        href={path}
+        target="_blank"
+        rel="noreferrer"
+        onMouseEnter={() => mouseListener(true)}
+        onMouseLeave={() => mouseListener(false)}
+      >
+        <Icon name={iconName} height="20" width="20" />
+      </a>
+      {hovered && <div className="circle-with-icon__qr"><div className="circle-with-icon__qr-arrow"/></div>}
+    </>
   );
 };
 
